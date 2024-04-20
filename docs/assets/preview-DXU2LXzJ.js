73978be16eb28bcd4267878e01ad8d124e6dd37d
Fr93562 <francois.mackowski@gmail.com>
2024-04-20 20:11:48 +0200

    feature(project): test deployment

---
+++ docs/assets/preview-DXU2LXzJ.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-K4EAMTCU-CMSyiV27.js","./iframe-D1mb9O81.js","./index-DH5ua8nC.js","./_commonjsHelpers-Cpj98o6Y.js","./react-18-iZ4baTDS.js","./index-D9uilScg.js","./index-DWohVFWU.js","./index-Dkj0J1ds.js","./index-8sIbGq5v.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./iframe-D1mb9O81.js";import"../sb-preview/runtime.js";const{global:s}=__STORYBOOK_MODULE_GLOBAL__;var _=Object.entries(s.TAGS_OPTIONS??{}).reduce((e,r)=>{let[t,o]=r;return o.excludeFromDocsStories&&(e[t]=!0),e},{}),d={docs:{renderer:async()=>{let{DocsRenderer:e}=await a(()=>import("./DocsRenderer-K4EAMTCU-CMSyiV27.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new e},stories:{filter:e=>{var r;return(e.tags||[]).filter(t=>_[t]).length===0&&!((r=e.parameters.docs)!=null&&r.disable)}}}};export{d as parameters};
