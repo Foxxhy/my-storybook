923bcab09bc19032e0726e57d32a8a12406d883d
Fr93562 <francois.mackowski@gmail.com>
2024-04-21 21:32:08 +0200

    fix(package): remove custom configuration

---
+++ docs/assets/Title.stories-C-pr8eb7.js
@@ -0,0 +1,45 @@
+import{j as z}from"./jsx-runtime-DQ32znRX.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";const q=M=>{const{type:N,style:i,transform:o,children:_}=M,j=`f-${N}`,I=i&&i!=="unset"?` f-style-${i}`:"",$=o&&o!=="unset"?` f-transform-${o}`:"";return z.jsx("p",{className:j+I+$,children:_||"Lorem Ipsum Factum"})};q.__docgenInfo={description:`Atom - Typography
+@param props: ITypography
+@returns - JSX`,methods:[],displayName:"Title",props:{type:{required:!0,tsType:{name:"union",raw:"'title-main' | 'title-section' | 'title-sub ' | 'subtitle-main' | 'subtitle-section' | 'subtitle-sub'",elements:[{name:"literal",value:"'title-main'"},{name:"literal",value:"'title-section'"},{name:"literal",value:"'title-sub '"},{name:"literal",value:"'subtitle-main'"},{name:"literal",value:"'subtitle-section'"},{name:"literal",value:"'subtitle-sub'"}]},description:""},style:{required:!1,tsType:{name:"union",raw:"'underline' | 'italic' | 'through' | 'unset'",elements:[{name:"literal",value:"'underline'"},{name:"literal",value:"'italic'"},{name:"literal",value:"'through'"},{name:"literal",value:"'unset'"}]},description:""},transform:{required:!1,tsType:{name:"union",raw:"'capitalize' | 'uppercase' | 'lowercase' | 'unset'",elements:[{name:"literal",value:"'capitalize'"},{name:"literal",value:"'uppercase'"},{name:"literal",value:"'lowercase'"},{name:"literal",value:"'unset'"}]},description:""},children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const J={title:"Atoms/Title",component:q,tags:["autodocs"],argTypes:{type:["text","label"]}},e={args:{type:"title-main",style:void 0,transform:void 0,children:"Lorem ipsum factum"}},t={args:{type:"title-section",style:void 0,transform:void 0,children:"Lorem ipsum factum"}},r={args:{type:"title-sub ",style:void 0,transform:void 0,children:"Lorem ipsum factum"}},s={args:{type:"subtitle-main",style:void 0,transform:void 0,children:"Lorem ipsum factum"}},n={args:{type:"subtitle-section",style:void 0,transform:void 0,children:"Lorem ipsum factum"}},a={args:{type:"subtitle-section",style:void 0,transform:void 0,children:"Lorem ipsum factum"}};var l,u,m;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    type: 'title-main',
+    style: undefined,
+    transform: undefined,
+    children: 'Lorem ipsum factum'
+  }
+}`,...(m=(u=e.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var c,d,p;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    type: 'title-section',
+    style: undefined,
+    transform: undefined,
+    children: 'Lorem ipsum factum'
+  }
+}`,...(p=(d=t.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var f,y,b;r.parameters={...r.parameters,docs:{...(f=r.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    type: 'title-sub ',
+    style: undefined,
+    transform: undefined,
+    children: 'Lorem ipsum factum'
+  }
+}`,...(b=(y=r.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var v,g,h;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    type: 'subtitle-main',
+    style: undefined,
+    transform: undefined,
+    children: 'Lorem ipsum factum'
+  }
+}`,...(h=(g=s.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var S,T,L;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    type: 'subtitle-section',
+    style: undefined,
+    transform: undefined,
+    children: 'Lorem ipsum factum'
+  }
+}`,...(L=(T=n.parameters)==null?void 0:T.docs)==null?void 0:L.source}}};var w,x,R;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    type: 'subtitle-section',
+    style: undefined,
+    transform: undefined,
+    children: 'Lorem ipsum factum'
+  }
+}`,...(R=(x=a.parameters)==null?void 0:x.docs)==null?void 0:R.source}}};const O=["TitleMain","TitleSection","TitleSub","SubtitleMain","SubtitleSection","SubtitleSub"];export{s as SubtitleMain,n as SubtitleSection,a as SubtitleSub,e as TitleMain,t as TitleSection,r as TitleSub,O as __namedExportsOrder,J as default};
