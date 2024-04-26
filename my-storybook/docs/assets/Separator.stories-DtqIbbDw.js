eec909da2176d7c57bcc5d1de75e37c898620b0c
Fr93562 <francois.mackowski@gmail.com>
2024-04-26 21:41:44 +0200

    feature(package): preparation for deployment on vercel

---
+++ my-storybook/docs/assets/Separator.stories-DtqIbbDw.js
@@ -0,0 +1,11 @@
+import{j as a}from"./jsx-runtime-DQ32znRX.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";const c=({type:l})=>{const m=`f-separator-${l}`;return a.jsx("section",{style:{height:"200px"},children:a.jsx("div",{className:m,children:" "})})};c.__docgenInfo={description:`- CardShadow
+@param props: ITypography
+@returns - JSX`,methods:[],displayName:"Separator",props:{type:{required:!0,tsType:{name:"union",raw:"'vertical' | 'horizontal'",elements:[{name:"literal",value:"'vertical'"},{name:"literal",value:"'horizontal'"}]},description:""}}};const g={title:"Atoms/Separator",component:c,tags:["autodocs"]},r={args:{type:"horizontal"}},e={args:{type:"vertical"}};var t,o,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    type: 'horizontal'
+  }
+}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var n,p,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    type: 'vertical'
+  }
+}`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const y=["Horizontal","Vertical"];export{r as Horizontal,e as Vertical,y as __namedExportsOrder,g as default};
